2596ca29711cc2ca2fd9b6a9e032fc332a71de6b
Renato Zanella <renatozr07@gmail.com>
2022-10-11 14:25:51 -0300

    feat: req2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -23,7 +24,11 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    news_urls = selector.css("h2.entry-title a::attr(href)").getall()
+
+    return news_urls
 
 
 # Requisito 3
